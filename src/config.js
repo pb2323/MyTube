f310b2f561b7ebf17663be817ffbe7a96e82ba44
Puneet <pbajaj0023@gmail.com>
2020-09-21 16:59:59 +0530

    ABC Change

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const key = {
-  API_KEY: "AIzaSyBj22-FRRjY9dIkWK_2F--vQzA-HPyVaFk",
+  API_KEY: "",
   CLIENT_ID:
-    "952751868830-olrfh41pu4iqpsjsu7oh5qhi27962b21.apps.googleusercontent.com",
+    "",
   CLIENT_SECRET: "qXwVblX6ikWNSD9LX59elLHv",
   BASE_URL: "https://www.googleapis.com/youtube/v3",
 };
